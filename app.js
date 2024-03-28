140209e490722f5acd310915633d94d864d8833b
Harsh raj <harshraj17087@gmail.com>
2024-03-28 13:01:23 +0530

    added CORS app.header

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*")
     res.header("Access-Control-Allow-Header","Origin,X-Requested-Width,Content-Type,Accept,Authorization")
     res.header("Access-Control-Allow-Credentials",true)
+    res.header("Access-Control-Allow-Method","PUT","POST","GET","DELETE")
     if(res.header==="OPTIONS"){
         res.header("Access-Control-Allow-Method","PUT","POST","GET","DELETE")
         return res.status(200).json()
