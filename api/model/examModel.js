d76c9324e0745d3871c6db1fd6a9bd47505a21e6
Harsh raj <harshraj17087@gmail.com>
2024-03-28 11:29:22 +0530

    id added for voucher and exam

--- api/model/examModel.js
+++ api/model/examModel.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const examSchema = Schema({
   _id:Schema.Types.ObjectId,  // _id keyword is mandatory
+  id:{type:String,required:true},
   exam:{type:String, require:true}
 });
 
--- api/model/voucherModel.js
+++ api/model/voucherModel.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const voucherSchema = Schema({
   _id:Schema.Types.ObjectId,  // _id keyword is mandatory
+  id:{type:String,require:true},
   voucher_code:{type:String, require:true}
 });
 
